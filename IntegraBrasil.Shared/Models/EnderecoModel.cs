4146414e470e6b07e9ef280fb4642a89d1bc044c
LucasAL11 <lucasluna1000@gmail.com>
2023-04-25 14:27:33 -0300

    Criado Model EnderecoModel.cs

---
+++ IntegraBrasil.Shared/Models/EnderecoModel.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.Json.Serialization;
+
+namespace IntegraBrasil.Shared.Models;
+
+public class EnderecoModel
+{
+    public class Coordinates
+    {
+        [JsonPropertyName("longitude")]
+        public string Longitude { get; set; }
+
+        [JsonPropertyName("latitude")]
+        public string Latitude { get; set; }
+    }
+
+    public class Location
+    {
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("coordinates")]
+        public Coordinates Coordinates { get; set; }
+    }
+
+    public class Endereco
+    {
+        [JsonPropertyName("cep")]
+        public string Cep { get; set; }
+
+        [JsonPropertyName("state")]
+        public string State { get; set; }
+
+        [JsonPropertyName("city")]
+        public string City { get; set; }
+
+        [JsonPropertyName("neighborhood")]
+        public string Neighborhood { get; set; }
+
+        [JsonPropertyName("street")]
+        public string Street { get; set; }
+
+        [JsonPropertyName("service")]
+        public string Service { get; set; }
+
+        [JsonPropertyName("location")]
+        public Location Location { get; set; }
+    }
+
+
+}
