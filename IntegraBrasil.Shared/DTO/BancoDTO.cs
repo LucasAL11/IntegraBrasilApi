eb86926ece24457dd90d8810f0169d2ff6e5b1c1
LucasAL11 <lucasluna1000@gmail.com>
2023-04-25 17:29:36 -0300

    Adiconado a DTO para retornas os bancos

--- IntegraBrasil.Shared/DTO/BancoDTO.cs
+++ IntegraBrasil.Shared/DTO/BancoDTO.cs
@@ -2,5 +2,11 @@
 
 public class BancoDTO
 {
-   
+    public string Ispb { get; set; }
+    
+    public string Nome { get; set; }
+    
+    public int Codigo { get; set; }
+    
+    public string NomeCompleto { get; set; }
 }
