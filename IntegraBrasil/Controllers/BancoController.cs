0317d311007aaa21760c6545a81f619af56c02e3
LucasAL11 <lucasluna1000@gmail.com>
2023-04-25 17:29:04 -0300

    [ADD] Adiconado a controller com a função de buscar todos os bancos

--- IntegraBrasil/Controllers/BancoController.cs
+++ IntegraBrasil/Controllers/BancoController.cs
@@ -1,8 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net;
+using IntegraBrasil.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace IntegraBrasil.Controllers;
 
+[ApiController]
+[Route("api/v1/banco")]
 public class BancoController : ControllerBase
 {
-    
+    private readonly IBancoServices _services;
+
+    public BancoController(IBancoServices service)
+    {
+        _services = service;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> BuscarEndereco()
+    {
+        var response = await _services.BuscarTodosBancos();
+
+        return response.CodigoHttp == HttpStatusCode.OK
+            ? Ok(response.DadosRetorno)
+            : StatusCode((int)response.CodigoHttp, response.ErroRetorno);
+    }
 }
