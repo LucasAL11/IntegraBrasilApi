8e1ba286949b0c2c1ee386e61fe2ae49932818ad
LucasAL11 <lucasluna1000@gmail.com>
2023-04-25 17:33:31 -0300

    [Update] Ajustado a interface para retornar uma lista

--- IntegraBrasil.Core/Interfaces/IBrasilApiService.cs
+++ IntegraBrasil.Core/Interfaces/IBrasilApiService.cs
@@ -6,6 +6,6 @@ namespace IntegraBrasil.Core.Interfaces;
 public interface IBrasilApiService
 {
     Task<GenericDTO<EnderecoModel>> BuscarEnderecoPorCep(string cep);
-    Task<GenericDTO<BancoModel>> BuscarTodosBancos();
+    Task<GenericDTO<List<BancoModel>>> BuscarTodosBancos();
     Task<GenericDTO<BancoModel>> BuscarBancoPorCodigo();
 }
