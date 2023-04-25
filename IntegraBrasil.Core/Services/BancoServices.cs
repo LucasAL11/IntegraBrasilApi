d409c1352a5a81bd637aec1cfcfeb8fb20f7f0a9
LucasAL11 <lucasluna1000@gmail.com>
2023-04-25 17:31:45 -0300

    [ADD] Criado um serviço que consome a o serviço BrasilApiService.cs e retorna uma lista de Instiituições bancarias

---
+++ IntegraBrasil.Core/Services/BancoServices.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using IntegraBrasil.Core.Interfaces;
+using IntegraBrasil.Shared.DTO;
+
+namespace IntegraBrasil.Core.Services;
+
+public class BancoServices : IBancoServices
+{
+    private readonly IMapper _mapper;
+    private readonly IBrasilApiService _brasilApiService;
+
+    public BancoServices(IMapper mapper, IBrasilApiService brasilApiService)
+    {
+        _mapper = mapper;
+        _brasilApiService = brasilApiService;
+    }
+
+    public async Task<GenericDTO<List<BancoDTO>>> BuscarTodosBancos()
+    {
+        var bancos = await _brasilApiService.BuscarTodosBancos();
+        return _mapper.Map<GenericDTO<List<BancoDTO>>>(bancos);
+    }
+
+    public Task<GenericDTO<BancoDTO>> BuscarBancoPorCodigo()
+    {
+        throw new NotImplementedException();
+    }
+}
