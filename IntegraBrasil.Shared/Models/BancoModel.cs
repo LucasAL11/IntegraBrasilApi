8582719370a6053c6fd39e4a8f08c5e5601f90e0
LucasAL11 <lucasluna1000@gmail.com>
2023-04-25 17:30:15 -0300

    [ADD] Criado a model dos bancos

--- IntegraBrasil.Shared/Models/BancoModel.cs
+++ IntegraBrasil.Shared/Models/BancoModel.cs
@@ -4,5 +4,15 @@ namespace IntegraBrasil.Shared.Models;
 
 public class BancoModel
 {
-   
+    [JsonPropertyName("ispb")]
+    public string? Ispb { get; set; }
+
+    [JsonPropertyName("name")]
+    public string? Nome { get; set; }
+
+    [JsonPropertyName("code")]
+    public int? Codigo { get; set; }
+
+    [JsonPropertyName("fullName")]
+    public string? NomeCompleto { get; set; }
 }
